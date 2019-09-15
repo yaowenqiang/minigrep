002fd10d92a342b69b6c99d4df857711b30b45c4
yaowenqiang <yaowenqiang111@163.com>
2019-09-16 00:18:39 +0800

    read file contents from arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 use std::env;
+use std::fs;
 fn main() {
+    let a:i32 = 1;
+    println!("{}", a);
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!(" In file  {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+
+
+
 }
