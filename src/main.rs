5d66005ccde456780de1ea1e587d8c7842c36bed
yaowenqiang <yaowenqiang111@163.com>
2019-09-18 02:33:53 +0800

    refactor config

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
     let contents = fs::read_to_string(config.filename)
@@ -19,8 +19,14 @@ struct Config {
     query: String,
     filename: String,
 }
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config{query, filename}
+
+impl Config {
+    fn new (args: &[String]) -> Config {
+        if args.len( ) < 3 {
+            panic!("not enough arguments!");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config{query, filename}
+    }
 }
