4adda936085b01fdbf7812525eee984078b9c760
yaowenqiang <yaowenqiang111@163.com>
2019-09-18 02:47:56 +0800

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use std::env;
 use std::fs;
+use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments:{}", err);
+        process::exit(1);
+    });
+
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
+
+    let contents = fs::read_to_string(&config.filename).unwrap_or_else(|err| {
+        err.to_string()
+    });
 
     println!("With text:\n{}", contents);
 
@@ -21,12 +28,13 @@ struct Config {
 }
 
 impl Config {
-    fn new (args: &[String]) -> Config {
+    fn new (args: &[String]) -> Result<Config, &'static str> {
         if args.len( ) < 3 {
-            panic!("not enough arguments!");
+            //panic!("not enough arguments!");
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config{query, filename}
+        Ok(Config{query, filename})
     }
 }
