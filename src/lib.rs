ff4978f4295ae53cbecc99b63fe880f5aabce83e
yaowenqiang <yaowenqiang111@163.com>
2019-09-20 02:00:29 +0800

    add a failed test

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,28 @@ use std::fs;
 use std::error::Error;
 
 
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+    #[test]
+
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive
+Pick three.";
+        assert_eq!(
+            vec!["safe, fast, productive"],
+            search(query, contents)
+            );
+    }
+}
 pub struct Config {
     pub query: String,
     pub filename: String,
