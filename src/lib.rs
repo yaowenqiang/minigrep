fee1fd0e74c5665279fc827e1f849d762480e210
yaowenqiang <yaowenqiang111@163.com>
2019-09-20 02:04:02 +0800

    implement search function and pass the test

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,13 @@ use std::error::Error;
 
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 #[cfg(test)]
