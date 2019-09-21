64193ebff16df34b57d441fae05c0c20612f7cd2
yaowenqiang <yaowenqiang111@163.com>
2019-09-21 20:59:23 +0800

    add case insenstive search function and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -12,6 +13,17 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    println!("{}", query);
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
 #[cfg(test)]
 
 mod tests {
@@ -29,10 +41,40 @@ Pick three.";
             search(query, contents)
             );
     }
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive
+Pick three.
+Duct tape
+";
+        assert_eq!(
+            vec!["safe, fast, productive"],
+            search(query, contents)
+            );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust
+safe, fast, productive
+Pick three.
+Trust me
+";
+        assert_eq!(
+            vec!["Rust","Trust me"],
+            search_case_insensitive(query, contents)
+            );
+    }
 }
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -43,7 +85,11 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config{query, filename})
+        //$env:CASE_INSENSITIVE=1 powershell
+        //cargo run to poem.txt
+        //CASE_INSENSITIVE=1 cargo run to poem.txt  linux or mac
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config{query, filename, case_sensitive})
     }
 }
 
@@ -51,11 +97,25 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 //        .expect("something went wrong reading the file");
     //println!("With text:\n{}", contents);
+    
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
 
+    };
+    /*
     for line in search(&config.query, &contents) {
         println!("{}", line);
+    }
+    */
+
+    for line in results {
+        println!("{}", line)
     }
 
     Ok(())
 
 }
+
+
