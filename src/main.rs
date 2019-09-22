8f0fa9324129818242c9e644b69fcc30007d4734
yaowenqiang <yaowenqiang111@163.com>
2019-09-23 00:31:09 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,13 @@ fn main() {
 
         process::exit(1)
     }
-
+    
+    let simulated_user_specified_value = 10;
+    let simplated_random_number = 7;
+    generate_workout(
+        simulated_user_specified_value,
+        simplated_random_number
+    );
     simulated_expensive_calculation(10);
 
 
@@ -34,4 +40,29 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("calculating slowly ...");
     thread::sleep(Duration::from_secs(2));
   intensity
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_result = simulated_expensive_calculation(intensity);
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            expensive_result
+        );
+        println!(
+            "Next, do {} situps!",
+            expensive_result
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result
+            );
+        }
+    } 
+
+
 }
