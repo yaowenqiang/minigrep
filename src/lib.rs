a92a2a3fe10c6e40d3e3e77fee7770dcd8e61ccd
yaowenqiang <yaowenqiang111@163.com>
2019-09-25 02:02:12 +0800

    add  closure struct demo

--- src/lib.rs
+++ src/lib.rs
@@ -77,6 +77,36 @@ pub struct Config {
     pub case_sensitive: bool,
 }
 
+pub struct Cacher<T> 
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>
+}
+
+impl <T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    pub fn new(calculation: T) -> Cacher<T>
+    {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    pub fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
+}
+
 impl Config {
     pub fn new (args: &[String]) -> Result<Config, &'static str> {
         if args.len( ) < 3 {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::process;
 use minigrep;
+use minigrep::Cacher;
 use minigrep::Config;
 use std::time::Duration;
 use std::thread;
@@ -33,6 +34,13 @@ fn main() {
     //simulated_expensive_calculation(10);
     //expensive_closure(10)
 
+    let example_closure = |x|x;
+    let s = example_closure(String::from("Hello"));
+    //let n = example_closure(5); error
+
+
+    
+
 
 }
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
@@ -42,22 +50,30 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
+    //let expensive_closure = |num: u32| -> ui2 {
     let expensive_closure = |num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
     };
     //let expensive_result = simulated_expensive_calculation(intensity);
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
             //expensive_result
-            expensive_closure(intensity)
+            //expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
         println!(
             "Next, do {} situps!",
             //expensive_result
-            expensive_closure(intensity)
+            //expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
